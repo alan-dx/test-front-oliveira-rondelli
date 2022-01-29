478dfdd646b092f044c04e7e0bff8cc592dea272
Alan Almeida <alanalmeida.emp@gmail.com>
2022-01-29 04:36:48 -0300

    feat: install next-pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public'
+  }
+})
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,15 @@ export default class MyDocumet extends Document {
       return (
         <Html>
         <Head>
+          <meta name='application-name' content="Plano Gestor" />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content="Plano Gestor" />
+          <meta name='description' content="An application to manage your expenses and gains" />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='theme-color' content='#F5F5F5' />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Sora:wght@400;600&display=swap" rel="stylesheet" />
