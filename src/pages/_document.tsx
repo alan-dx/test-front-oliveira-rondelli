c419435d1234fc3fee1c05f937bbf99fc97a2a07
Alan Almeida <alanalmeida.emp@gmail.com>
2022-01-29 04:59:07 -0300

    fix: config _document.tsx PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,7 +18,8 @@ export default class MyDocumet extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Sora:wght@400;600&display=swap" rel="stylesheet" />
 
-          {/* <link rel="shortcut icon" href="/favicon.png" type="image/png" /> */}
+          <link rel='manifest' href='/manifest.json' />
+
         </Head>
         <Main />
         <NextScript />
